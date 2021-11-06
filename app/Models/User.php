d7d3424084060dde2093905bb20bfdd9c2eb8062
arthr <arthrmrs@gmail.com>
2021-11-06 12:07:24 -0300

    relacionamento model User

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,14 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function tickets()
+    {
+        return $this->hasMany(Ticket::class);
+    }
+
+    public function pedidos()
+    {
+        return $this->hasMany(Pedido::class);
+    }
 }
